c16d43349f66f35210c3cecca265311e496a358b
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2024-02-26 07:09:57 -0500

    test for 3 and 4

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Base Model module"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Base Model class"""
+    def __init__(self, *args, **kwargs):
+        """Init function"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+
+    def __str__(self):
+        """Str function"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id,
+                                     self.__dict__)
+
+    def save(self):
+        """Save function"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dict = self.__dict__
+        dict["__class__"] = self.__class__.__name__
+        dict["created_at"] = self.created_at.isoformat()
+        dict["updated_at"] = self.updated_at.isoformat()
+        return dict
