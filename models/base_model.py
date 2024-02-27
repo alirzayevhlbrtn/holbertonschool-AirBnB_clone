2676c7056f37bd6444157387b290d13fee482881
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2024-02-27 13:47:46 +0400

    if else error fixed in kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -15,9 +15,6 @@ class BaseModel:
         """
         Init function
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at":
@@ -26,6 +23,11 @@ class BaseModel:
                     value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key != "__class__":
                     setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
