88a03c0b697448e68f0bf599097a8c1f05f16e2b
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2024-02-27 18:04:08 +0400

    fix of tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -20,37 +20,6 @@ class TestBaseModel(unittest.TestCase):
 
         FileStorage.__objects == {}
 
-    def test_check_type(self):
-        self.assertIsInstance(FileStorage.__file_path, str)
-        self.assertIsInstance(FileStorage.__objects, dict)
-
-    def test_storage_all(self):
-        obj1 = BaseModel()
-        obj2 = BaseModel()
-        storage.new(obj1)
-        storage.new(obj2)
-
-        allobjects = storage.all()
-
-        self.assertIn(obj1, allobjects.values())
-        self.assertIn(obj2, allobjects.values())
-
-    def test_storage_new(self):
-        obj = BaseModel()
-        storage.new(obj)
-
-        self.assertIn(obj, storage.all().values())
-
-    def test_storage_save(self):
-        obj = BaseModel()
-
-        storage.new(obj)
-        storage.save()
-
-        with open("file.json", "r") as f:
-            text = f.read()
-            self.assertIn("BaseModel." + obj.id, text)
-
     def test_id_generation(self):
         base_model = BaseModel()
         self.assertIsNotNone(base_model.id)
--- tests/test_models/test_engine/test_base_model.py
+++ tests/test_models/test_engine/test_base_model.py
@@ -50,38 +50,3 @@ class TestBaseModel(unittest.TestCase):
         with open("file.json", "r") as f:
             text = f.read()
             self.assertIn("BaseModel." + obj.id, text)
-
-    def test_id_generation(self):
-        base_model = BaseModel()
-        self.assertIsNotNone(base_model.id)
-        self.assertIsInstance(base_model.id, str)
-
-    def test_created_at(self):
-        base_model = BaseModel()
-        self.assertIsNotNone(base_model.created_at)
-        self.assertIsInstance(base_model.created_at, datetime)
-
-    def test_save_method(self):
-        base_model = BaseModel()
-        previous_updated_at = base_model.updated_at
-        base_model.save()
-        self.assertNotEqual(previous_updated_at, base_model.updated_at)
-
-    def test_save_method_with_storage(self):
-        base_model = BaseModel()
-        base_model.name = "My_First_Model"
-        base_model.my_number = 89
-        base_model.save()
-        self.assertTrue(os.path.exists("file.json"))
-        self.assertIn("BaseModel." + base_model.id, storage.all())
-
-    def test_to_dict_method(self):
-        base_model = BaseModel()
-        obj_dict = base_model.to_dict()
-        self.assertIsInstance(obj_dict, dict)
-        self.assertEqual(obj_dict["__class__"], "BaseModel")
-
-    def test_str_method(self):
-        base_model = BaseModel()
-        expected_string = f"[BaseModel] ({base_model.id}) {base_model.__dict__}"
-        self.assertEqual(str(base_model), expected_string)
