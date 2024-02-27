140ec9d485e408d847771b19c970a8bf2ebeb912
alirzayevhlbrtn <elirzayevuzeyir1@gmail.com>
2024-02-27 19:01:38 +0400

    save changes for later

--- console.py
+++ console.py
@@ -3,7 +3,8 @@
 Console of HBNB
 """
 import cmd
-
+import json
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -34,6 +35,72 @@ class HBNBCommand(cmd.Cmd):
         Passing empty line
         """
         pass
+
+    def do_create(self, args):
+        if not args:
+            print("** class name missing **")
+            return
+        arglist = args.split()
+        class_name = arglist[0]
+        if class_name != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        if class_name == "BaseModel":
+            new = BaseModel()
+            new.save()
+            print(new.id)
+
+    def do_show(self, args):
+        if not args:
+            print("** class name missing **")
+            return
+        arglist = args.split()
+        class_name = arglist[0]
+        if class_name != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        if len(arglist) < 2:
+            print("** instance id missing **")
+            return
+        ins_id = arglist[1]
+        key = "{}.{}".format(class_name, ins_id)
+        all_ins = BaseModel.to_dict()
+        if key in all_ins:
+            print(all_ins[key])
+        else:
+            print("** no instance found **")
+
+    def do_destroy(self, args):
+        if not args:
+            print("** class name missing **")
+            return
+        arglist = args.split()
+        class_name = arglist[0]
+        if class_name != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        if len(arglist) < 2:
+            print("** instance id missing **")
+            return
+        insid = arglist[1]
+        key = "{}.{}".format(class_name, ins_id)
+        all_ins = BaseModel.to_dict()
+        if key in all_ins:
+            del all_ins[key]
+            BaseModel.save()
+        else:
+            print("** no instance found **")
+            return
+
+    def do_all(self, args):
+        all_ins = BaseModel.to_dict()
+        arglist = args.split()
+        class_name = arglist[0]
+        if class_name != "BaseModel":
+            print("** class doesn't exist **")
+            return
+        elif not args or class_name == "BaseModel":
+            pass
 
 
 if __name__ == '__main__':
